fd6d800330f6a591146f526ee518cec58d9d39a9
Sam Phippen <samphippen@googlemail.com>
2015-03-14 13:06:03 +0000

    Fix times script to not be shit

--- times.py
+++ times.py
@@ -1,39 +1,40 @@
 import csv
 from collections import Counter
-from matplotlib import pyplot as plt
 
-reader = csv.reader(open("responses.csv"))
-reader.next()
+if __name__ == "__main__":
 
-build = Counter()
+    reader = csv.reader(open("responses.csv"))
+    reader.next()
 
-for r in reader:
-    for time in r[6].split(","):
-        time = time.strip()
-        build[time] += 1
+    build = Counter()
+
+    for r in reader:
+        for time in r[6].split(","):
+            time = time.strip()
+            build[time] += 1
 
 
-def sorter(key):
-    if "Thursday" in key:
-        return 0
-    elif "Friday" in key:
-        if "9am" in key:
-            return 1000
-        else:
-            return 1001
-    elif "Saturday" in key:
-        if "8am" in key:
-            return 2000
-        else:
-            return 2001
-    elif "Sunday" in key:
-        if "onwards" in key:
-            return 3500
-        return 3000
+    def sorter(key):
+        if "Thursday" in key:
+            return 0
+        elif "Friday" in key:
+            if "9am" in key:
+                return 1000
+            else:
+                return 1001
+        elif "Saturday" in key:
+            if "8am" in key:
+                return 2000
+            else:
+                return 2001
+        elif "Sunday" in key:
+            if "onwards" in key:
+                return 3500
+            return 3000
 
-ml = max([len(x) for x in build.keys()])+1
-for key in sorted(build.keys(), key=sorter):
-    value = build[key]
-    print str(key).rjust(ml), value
+    ml = max([len(x) for x in build.keys()])+1
+    for key in sorted(build.keys(), key=sorter):
+        value = build[key]
+        print str(key).rjust(ml), value
 
-plt.show()
+    plt.show()
